c98abb35b5e682d22ae6761aa5b3d0a220a127bd
Lee Chun On <jeffreyleeon@gmail.com>
2017-03-27 22:14:01 +0800

    Add a wildcard route and error route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/*', function(req, res, next) {
+  console.log('======This is the first route in router');
+  next();
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 module.exports = router;
